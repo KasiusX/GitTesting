81f2c1ae6152a340ad6571cd3733505eea5f68ab
Tomas Knor <tomas.knor159@gmail.com>
2021-03-14 09:39:31 +0100

    first commit

--- GitTestingDemo/Program.cs
+++ GitTestingDemo/Program.cs
@@ -6,7 +6,7 @@ namespace GitTestingDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, HI!");
         }
     }
 }
